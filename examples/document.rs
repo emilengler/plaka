bb13356c2544b31f50a6ed9ed6b7bed539d29f0b
Emil Engler <me@emilengler.com>
2023-10-29 16:31:55 +0100

    Finish a working parser prototype

--- examples/document.rs
+++ examples/document.rs
@@ -3,15 +3,13 @@
 use std::{fs::File, io::Read};
 
 use pest::Parser;
-use plaka::{Document, Rule};
+use plaka::Document;
 
 fn main() {
     let mut f = File::open("./examples/data/consensus").unwrap();
     let mut s = String::new();
     f.read_to_string(&mut s).unwrap();
 
-    let mut root = Document::parse(Rule::Document, &s).unwrap();
-    let document = root.next().unwrap();
-    assert_eq!(document.as_rule(), Rule::Document);
-    assert_eq!(document.into_inner().next().unwrap().as_rule(), Rule::Item);
+    let doc = Document::parse_str(&s).unwrap();
+    println!("{:#?}", doc);
 }
